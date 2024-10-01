1082b3c76c9a1205b3a41d61b8c815de7be28800
M Santosh <sunny962002@gmail.com>
2024-10-01 21:51:19 +0530

    Export Fixes

--- src/ColorPro5gContents/ColorPro5gComponents/Specifications.jsx
+++ src/ColorPro5gContents/ColorPro5gComponents/Specifications.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function Specifications() {
-  return <div style={{ height: "100svh", background: "#154206" }}></div>;
+  return <div style={{ height: "100svh", background: "#111" }}></div>;
 }
